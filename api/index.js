c90565cb20f39308e41e8895712ca9433261d23f
Shubham Narendra Patil <mr.sp3799@gmail.com>
2026-05-07 14:32:41 +0530

    fix

--- api/index.js
+++ api/index.js
@@ -1,5 +1,2 @@
 // Vercel serverless function entry point
-import { createRequire } from 'module';
-const require = createRequire(import.meta.url);
-const apiRouter = require('./router.cjs');
-export default apiRouter;
+module.exports = require('./router.cjs');
